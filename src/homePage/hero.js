2b1bb32d81241c0fd59c8ef437c7c7bce2a61b5b
Jla <Jessica.lair@hotmail.com>
2020-05-13 12:12:43 -0700

    added new photos

--- src/homePage/hero.js
+++ src/homePage/hero.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Angeli from '../assets/images/angeli.jpg';
+import AngeliTwo from '../assets/images/angeli3.jpg'
 
 const Hero = () => {
   return (
     <main>
    <div className="new-hero">
-    <img src={Angeli} alt="a photo of Angeli"/>
+    <img src={AngeliTwo} alt="a photo of Angeli"/>
     <h2><span>Angeli's Next Tattoo.</span></h2>
     </div>
     </main>
